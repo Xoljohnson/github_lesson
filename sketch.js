5fc9e7fcb285249f9fd3fdcd9b123ebeaf8910ca
Xol Johnson <xoljohnson2@gmail.com>
2024-04-18 01:44:29 +0000

    added old code from p5

--- sketch.js
+++ sketch.js
@@ -1,40 +1,21 @@
+let img;
+function preload() {
+  img = loadImage(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2DpStvkbaMI60CKoakggpu50Kmvs-2yH_PW6e0Gnh7nNUXNT_U_kFpns3QKfSeDKW74I:https://i.redd.it/vmbyuwcnw0y51.png&usqp=CAU"
+  );
+}
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(220);
-    background("green")
-    strokeWeight(5)
-    fill('white')
-    ellipse(175, 300, 50, 30)
-    ellipse(225, 300, 50, 30)
-    fill('yellow')
-    ellipse(165, 250, 30, 50)
-    ellipse(235, 250, 30, 50)
-    rect(160, 210, 80, 80)
-    
-    
-    
-    triangle(165, 220, 200, 230, 185, 240)
-    triangle(235, 220, 200, 230, 215, 240)
-    fill('white')
-    strokeWeight(3)
-    ellipse(200, 242, 10, 10)
-    strokeWeight(5)
-    ellipse(170, 115, 45, 80)
-    ellipse(230, 115, 45, 80)
-    ellipse(200, 180, 120, 100)
-    strokeWeight(0)
-  
-    ellipse(175, 140, 35, 25)
-    ellipse(225, 140, 35, 25)
-    fill('black')
-    ellipse(180, 185, 10, 10)
-    ellipse(220, 185, 10, 10)
-    strokeWeight(5)
-    line(195, 200, 205, 205);
-    line(195, 205, 205, 200);
-    
-    
-  }
+  createCanvas(400, 400, WEBGL);
+}
+function draw() {
+  background(0);
+  pointLight(250, 250, 550, frameCount % 400, -frameCount % 400, 90 * 2.5);
+  noStroke();
+  rotateY(frameCount * 0.03);
+  specularMaterial(250);
+  sphere(90);
+  shininess(100);
+  texture(img);
+  describe("planet obama")
+}
